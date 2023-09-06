58365c5673d5b104083672f035fe8081d95ebdbb
ManuelFBejaranoAA <manubejaardi17@gmail.com>
2023-09-06 13:58:15 -0500

    .

--- Php_Base/send.php
+++ Php_Base/send.php
@@ -2,20 +2,20 @@
 
     include("conexion.php");
 
-    if(isset($_POST["send"])) {
+    if(isset($_POST["send"])){
 
         if(
             strlen($_POST["name"]) >= 1 &&
             strlen($_POST["password"]) >= 1&&
             strlen($_POST["email"]) >= 1 &&
             strlen($_POST["phone"]) >= 1
-        ) {
+        ){
             $name = trim($_POST["name"]);
             $password = trim($_POST["password"]);
             $email = trim($_POST["email"]);
             $phone = trim($_POST["phone"]);
             $fecha = date("d/m/y");
-            $consulta = "INSERT INTO cliente(Nombre, contraseña, Correo, Telefono, Fecha)
+            $consulta = "INSERT INTO cliente(Nombre, Contraseña, Correo, Telefono, Fecha)
                         VALUES('$name', '$password', '$email', '$phone', '$fecha')";
             $resultado = mysqli_query($conex, $consulta);  
             if($resultado) {
