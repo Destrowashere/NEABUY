7bc77b863288dea2ef74c2b8fc0411a96c8b4bd9
ManuelFBejaranoAA <139181263+ManuelFBejaranoAA@users.noreply.github.com>
2023-09-06 13:28:21 -0500

    Base de datos nearbuy

--- nearbuy.sql
+++ nearbuy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-08-2023 a las 22:25:34
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.0.28
+-- Tiempo de generación: 06-09-2023 a las 20:26:07
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,83 +24,111 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `administrador`
---
-
-CREATE TABLE `administrador` (
-  `Identificacion` int(12) NOT NULL,
-  `Nombre` varchar(30) NOT NULL,
-  `Apellido` varchar(30) NOT NULL,
-  `Edad` int(2) NOT NULL,
-  `Correo` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `cliente`
 --
 
 CREATE TABLE `cliente` (
-  `identificacion` int(12) NOT NULL,
-  `Nombre` varchar(30) NOT NULL,
-  `Apellido` varchar(30) NOT NULL,
-  `Edad` int(2) NOT NULL,
-  `Direccion` varchar(50) NOT NULL,
-  `Metodo_Pago` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id_Cliente` varchar(30) NOT NULL,
+  `Nombre` varchar(30) DEFAULT NULL,
+  `Contraseña` varchar(50) DEFAULT NULL,
+  `Correo` varchar(50) NOT NULL,
+  `Telefono` int(20) NOT NULL,
+  `Fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_Cliente`, `Nombre`, `Contraseña`, `Correo`, `Telefono`, `Fecha`) VALUES
+('', 'Test1', 'test1sumerce', 'test1@gmail.com', 32154751, '2004-09-23');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `repartidor`
+-- Estructura de tabla para la tabla `detalle`
 --
 
-CREATE TABLE `repartidor` (
-  `Identificacion` int(2) NOT NULL,
-  `Nombre` varchar(30) NOT NULL,
-  `Apellido` varchar(30) NOT NULL,
-  `Edad` int(2) NOT NULL,
-  `N_Matricula` varchar(7) NOT NULL,
-  `Direccion` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `detalle` (
+  `Numero_Detalle` varchar(30) NOT NULL,
+  `Id_Factura` varchar(30) NOT NULL,
+  `Id_Producto` varchar(30) NOT NULL,
+  `Cantidad` varchar(30) NOT NULL,
+  `Precio` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tendero`
+-- Estructura de tabla para la tabla `factura`
 --
 
-CREATE TABLE `tendero` (
-  `Identificacion` int(12) NOT NULL,
+CREATE TABLE `factura` (
+  `Num_Factura` varchar(30) NOT NULL,
+  `Id_Cliente` varchar(30) NOT NULL,
+  `Fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
   `Nombre` varchar(30) NOT NULL,
-  `Apellido` varchar(30) NOT NULL,
-  `Edad` int(2) NOT NULL,
-  `Direccion` varchar(50) NOT NULL,
-  `Nom_tienda` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Precio` varchar(30) NOT NULL,
+  `Stock` int(30) NOT NULL,
+  `Id_Producto` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `administrador`
+-- Indices de la tabla `cliente`
 --
-ALTER TABLE `administrador`
-  ADD PRIMARY KEY (`Identificacion`);
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_Cliente`);
 
 --
--- Indices de la tabla `repartidor`
+-- Indices de la tabla `detalle`
 --
-ALTER TABLE `repartidor`
-  ADD PRIMARY KEY (`Identificacion`);
+ALTER TABLE `detalle`
+  ADD PRIMARY KEY (`Numero_Detalle`),
+  ADD KEY `Id_Factura` (`Id_Factura`),
+  ADD KEY `Id_Producto` (`Id_Producto`);
 
 --
--- Indices de la tabla `tendero`
+-- Indices de la tabla `factura`
 --
-ALTER TABLE `tendero`
-  ADD PRIMARY KEY (`Identificacion`);
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`Num_Factura`),
+  ADD KEY `Id_Cliente` (`Id_Cliente`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`Id_Producto`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle`
+--
+ALTER TABLE `detalle`
+  ADD CONSTRAINT `detalle_ibfk_1` FOREIGN KEY (`Id_Factura`) REFERENCES `factura` (`Num_Factura`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detalle_ibfk_2` FOREIGN KEY (`Id_Producto`) REFERENCES `producto` (`Id_Producto`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`Id_Cliente`) REFERENCES `cliente` (`id_Cliente`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
