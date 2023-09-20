af6fbf7b097254bd47b67d09986b3328e8589aef
ManuelFBejaranoAA <manubejaardi17@gmail.com>
2023-09-20 14:24:36 -0500

    css

--- Poductoconet/index.php
+++ Poductoconet/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='tendero.css'>
+    <link rel='stylesheet' href='styles.css'>
     <title>Agregar producto</title>
   </head>
   <body>
@@ -50,3 +50,4 @@
     include("send.php");
 ?>
       </body>
+      </html>
