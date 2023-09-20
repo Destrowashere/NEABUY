d844cce6eb0962219890e450975d25345603f0bf
Destrowashere <felixsantiagosantafe2017@gmail.com>
2023-09-20 15:34:42 -0500

    pin pan encriptado

--- Php_Base/send.php
+++ Php_Base/send.php
@@ -1,45 +1,45 @@
 <?php
+include("conexion.php");
 
-    include("conexion.php");
+if(isset($_POST["send"])){
 
-    if(isset($_POST["send"])){
-
-        if(
-            strlen($_POST["name"]) >= 1 &&
-            strlen($_POST["lastname"]) >= 1 &&
-            strlen($_POST["password"]) >= 1&&
-            strlen($_POST["email"]) >= 1 &&
-            strlen($_POST["phone"]) >= 1 
-         
-        ){
-            $name = trim($_POST["name"]);
-            $lastname = trim($_POST["lastname"]);
-            $password = trim($_POST["password"]);
-            $Cedula = trim($_POST["cedula"]);
-            $email = trim($_POST["email"]);
-            $phone = trim($_POST["phone"]);
-            $direct = trim($_POST["direct"]);
-            $fecha = date("y/m/d");
-            $contrasena_encriptada = password_hash($password, PASSWORD_DEFAULT);
-            $consulta = "INSERT INTO clientes( id_Cliente,Nombre, Apellido, ContraUser, Correo, Telefono, Direccion, Fecha,Cedula)
-                        VALUES('','$name','$lastname', '$password', '$email', '$phone','$direct', '$fecha','$Cedula')";
-            $resultado = mysqli_query($conex, $consulta);  
-            if($resultado) {
-                ?>
-                    <h3 class="success">Tu registro se ha completado</h3>
-                <?php
-            } else{
-                ?>
-                    <h3 class="error">Ocurrio un error</h3>
-                <?php
-            }
-        }else{
+    if(
+        strlen($_POST["name"]) >= 1 &&
+        strlen($_POST["lastname"]) >= 1 &&
+        strlen($_POST["password"]) >= 1 &&
+        strlen($_POST["email"]) >= 1 &&
+        strlen($_POST["phone"]) >= 1
+    ){
+        $name = trim($_POST["name"]);
+        $lastname = trim($_POST["lastname"]);
+        $password = trim($_POST["password"]);
+        $Cedula = trim($_POST["cedula"]);
+        $email = trim($_POST["email"]);
+        $phone = trim($_POST["phone"]);
+        $direct = trim($_POST["direct"]);
+        $fecha = date("y/m/d");
+        
+        // Aplicar hash a la contraseña
+        $contrasena_encriptada = password_hash($password, PASSWORD_DEFAULT);
+        
+        $consulta = "INSERT INTO clientes (id_Cliente, Nombre, Apellido, Contrasena, Correo, Telefono, Direccion, Fecha, Cedula)
+                    VALUES ('', '$name', '$lastname', '$contrasena_encriptada', '$email', '$phone', '$direct', '$fecha', '$Cedula')";
+                    
+        $resultado = mysqli_query($conex, $consulta);  
+        
+        if($resultado) {
             ?>
-                    <h3 class="success">Llena todos los campos</h3>
-                <?php
+                <h3 class="success">Tu registro se ha completado</h3>
+            <?php
+        } else{
+            ?>
+                <h3 class="error">Ocurrió un error</h3>
+            <?php
         }
-
-
+    } else{
+        ?>
+            <h3 class="success">Llena todos los campos</h3>
+        <?php
+    }
 }
-
 ?>
