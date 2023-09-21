54116e85ebbdb1aed49f7da9e9be717c53b96311
ManuelFBejaranoAA <manubejaardi17@gmail.com>
2023-09-20 21:02:59 -0500

    mi tienda 2

--- MiTienda/script.js
+++ MiTienda/script.js
@@ -1,51 +1,95 @@
-// Lista de productos
-const products = [
-    { id: 1, name: 'Producto 1', price: 10.99 },
-    // Agrega los otros productos aquí
-];
+function E(selector, parent) {
+    if(selector instanceof HTMLElement){
+         return selector;
+    }
+       
+    
+    return (parent || document).querySelectorAll(selector);
+ }
 
-// Carrito de compras
-const cart = [];
+ function hasClass(element, className) {
+    return element.classList.contains(className);
+ }
 
-// Función para agregar un producto al carrito
-function addToCart(productId) {
-    const product = products.find(p => p.id === productId);
-    if (product) {
-        cart.push(product);
-        updateCart();
+ function radioClass(element, className) {
+    E("." + className).forEach((elem)=> 
+    elem.classList.remove(className));
+    element.classList.toggle(className);
+}
+
+function tabs(nav) {
+    let navElem = E(nav)[0];
+
+    navElem.addEventListener("click", (e) => {
+        let target = e.target;
+
+        if(hasClass(target, "tab"))
+            radioClass(target, "active");
+        
+        let linkedTab = E("." + target.id)[0];
+
+        radioClass(linkedTab, "visible");
+
+        
+    });
+
+    let active = E(".tab.active")[0];
+    if (active) {
+        radioClass(E("."+active.id)[0], "visible");
     }
 }
 
-// Función para actualizar el carrito
-function updateCart() {
-    const cartItems = document.getElementById('cart-items');
-    const cartTotal = document.getElementById('cart-total');
+tabs(".menu-nav")
 
-    cartItems.innerHTML = '';
-    let total = 0;
 
-    cart.forEach(item => {
-        const listItem = document.createElement('li');
-        listItem.className = 'cart-item';
-        listItem.innerHTML = `${item.name} - $${item.price}`;
-        cartItems.appendChild(listItem);
-        total += item.price;
-    });
+let loadMoreBtn1 = document.querySelector('#load-more-1');
+let currentItem1 = 4;
 
-    cartTotal.textContent = `$${total.toFixed(2)}`;
+loadMoreBtn1.onclick = () => {
+    let boxes = [...document.querySelectorAll('.box-container-1 .box-1')];
+    for(var i = currentItem1; i < currentItem1 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem1 += 4;
+    if(currentItem1 >= boxes.length) {
+        loadMoreBtn1.style.display= 'none'
+    }
 }
 
-// Event listeners para botones de agregar al carrito
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
-addToCartButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const productId = parseInt(button.getAttribute('data-product-id'));
-        addToCart(productId);
-    });
-});
 
-// Event listener para el botón de pago
-const checkoutButton = document.getElementById('checkout-button');
-checkoutButton.addEventListener('click', () => {
-    alert('Proceso de pago no implementado en este ejemplo.');
-});
+let loadMoreBtn2 = document.querySelector('#load-more-2');
+let currentItem2 = 4;
+
+loadMoreBtn2.onclick = () => {
+    let boxes= [...document.querySelectorAll('.box-container-2 .box-2')];
+    for(var i = currentItem2; i < currentItem2 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem2 += 4;
+    if(currentItem2 >= boxes.length) {
+        loadMoreBtn2.style.display= 'none'
+    }
+}
+
+let loadMoreBtn3 = document.querySelector('#load-more-3');
+let currentItem3 = 4;
+
+loadMoreBtn3.onclick = () => {
+    let boxes= [...document.querySelectorAll('.box-container-3 .box-3')];
+    for(var i = currentItem3; i < currentItem3 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem3 += 4;
+    if(currentItem3 >= boxes.length) {
+        loadMoreBtn3.style.display= 'none'
+    }
+}
+
+ 
+
+
+
+
+
+
+
