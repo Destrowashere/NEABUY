254a7afff72a6fe2469721ea3fcc585f24091772
ManuelFBejaranoAA <manubejaardi17@gmail.com>
2023-11-10 12:18:11 -0500

    Inicio funcional

---
+++ Inicio de sesion/inici.php
@@ -0,0 +1,44 @@
+<?php 
+
+session_start();
+
+$conexion = new mysqli("localhost", "root", "", "neabuybonito");
+
+$error_message = "";
+
+if (!empty($_POST["submit"])) {
+    if (empty($_POST["Correo"]) || empty($_POST["contrasena"])) {
+        $error_message = "CAMPOS VACIOS";
+    } else {
+        $correo = $_POST["Correo"];
+        $contrasena = $_POST["contrasena"];
+        
+
+        $query = "SELECT * FROM claves WHERE Correo ='$correo'";
+        $result = $conexion->query($query);
+
+        if ($result !== false) {
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $hashedPassword = $row['contrasena'];
+
+                if (password_verify($contrasena, $hashedPassword)) {
+                    $_SESSION['usuario_nombre'] = $row['correo'];
+                    $_SESSION['usuario_apellido'] = $row['contrasena'];
+                    header("location: index.html");
+                    exit();
+                } else {
+                    $error_message = "Contraseña incorrecta";
+                }
+            } else {
+                $error_message = "Usuario no existe";
+            }
+        } else {
+            $error_message = "Error en la consulta: " . $conexion->error;
+        }
+    }
+}
+?>
+
+        
+
--- Php_Base/conexion.php
+++ Php_Base/conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-    $conex = mysqli_connect("localhost", "root", "", "nearbuy");
+    $conex = mysqli_connect("localhost", "root", "", "neabuybonito");
 
 ?>
