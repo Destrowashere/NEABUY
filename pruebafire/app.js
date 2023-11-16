9f6a0b8109fd9f25b772188a187afa6356f70325
Destrowashere <felixsantiagosantafe2017@gmail.com>
2023-11-16 17:44:55 -0500

    firedick

---
+++ pruebafire/app.js
@@ -0,0 +1,16 @@
+ function registrar(){
+    var email = document.getElementById('email').value;
+    var contrasena = document.getElementById('contrasena').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, contrasena)
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+ }
