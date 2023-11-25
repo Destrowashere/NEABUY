7498817641dc7e856763f30048b459c1d8cdd952
Destrowashere <felixsantiagosantafe2017@gmail.com>
2023-11-25 09:50:22 -0500

    AAAAAAAAAAAA

--- Php_Base/index.php
+++ Php_Base/index.php
@@ -10,12 +10,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-<script src="https://www.google.com/recaptcha/api.js?render=6LeY1xspAAAAANGSMIJCNpQEediDDpztjC9V79of"></script>
-<script>
-   function onSubmit(token) {
-     document.getElementById("demo-form").submit();
-   }
- </script>
+
 </head>
 <body>
     
