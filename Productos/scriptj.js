cb51a9d9b66f47385ba14f37f447a9f7f3269fb7
Juan David Hernandez Guerrero <hernandezjuandavidguerrero@gmail.com>
2023-11-29 17:40:35 -0500

    asdd

--- Productos/scriptj.js
+++ Productos/scriptj.js
@@ -20,6 +20,10 @@ function mostrar_buscador(){
     bars_search.style.top = "80px";
     cover_ctn_search.style.display = "block";
     inputSearch.focus();
+
+    if(inputSearch.value === ""){
+        box_search.style.display = "none"
+    }
 }
 
 //funcion para ocultar buscador
@@ -27,6 +31,36 @@ function ocultar_buscador(){
     bars_search.style.top="-90px"
     cover_ctn_search.style.display="none"; 
     inputSearch.value= "";
+    box_search.style.display= "none";
+}
+
+//Creando filtrado de busqueda
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno)
+
+function buscador_interno(){
+
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+    for (i = 0; i < li.length; i++){
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+                li[i].style.display = "";
+                box_search.style.display = "block";
+
+                if(inputSearch.value === ""){
+                    box_search.style.display = "none"
+                }
+        }else{
+            li[i].style.display = "none"
+        }
+    }
+
 }
 
  function E(selector, parent) {
