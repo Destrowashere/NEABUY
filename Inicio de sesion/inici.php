f836704125e759c1bf1f228f66973801f61a741e
Juan David Hernandez Guerrero <hernandezjuandavidguerrero@gmail.com>
2023-12-01 17:25:24 -0500

    asd

--- Inicio de sesion/inici.php
+++ Inicio de sesion/inici.php
@@ -1,43 +1,62 @@
-<?php 
+<?php
 
 session_start();
 
 $conexion = new mysqli("localhost", "root", "", "nearbuybonitoo");
 
 $error_message = "";
-
 if (!empty($_POST["submit"])) {
+    // Validar campos vacíos
     if (empty($_POST["Correo"]) || empty($_POST["contrasena"])) {
-        $error_message = "CAMPOS VACIOS";
+      $error_message = "Por favor complete todos los campos.";
     } else {
-        $correo = $_POST["Correo"];
-        $contrasena = $_POST["contrasena"];
-        
-
-        $query = "SELECT * FROM claves WHERE Correo ='$correo'";
-        $result = $conexion->query($query);
-
-        if ($result !== false) {
-            if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc();
-                $hashedPassword = $row['contrasena'];
-
-                if (password_verify($contrasena, $hashedPassword)) {
-                    $_SESSION['usuario_nombre'] = $row['correo'];
-                    $_SESSION['usuario_apellido'] = $row['contrasena'];
-                    header("location:../index.html");
-                    exit();
-                } else {
-                    $error_message = "Contraseña incorrecta";
-                }
-            } else {
-                $error_message = "Usuario no existe";
-            }
+      $correo = $_POST["Correo"];
+      $contrasena = $_POST["contrasena"];
+  
+      // Validar correo y contraseña en la base de datos
+      $query = "SELECT * FROM claves WHERE Correo ='$correo'";
+      $result = $conexion->query($query);
+  
+      if ($result) {
+        if ($result->num_rows > 0) {
+          $row = $result->fetch_assoc();
+          $hashedPassword = $row['contrasena'];
+  
+          // Validar contraseña
+          if (!password_verify($contrasena, $hashedPassword)) {
+            ?>
+            <div class="error-message">
+              <p>Correo o contraseña incorrectos.</p>
+              <a href="inici.html">Volver a intentar</a>
+            </div>
+            <?php
+          } else {
+            $_SESSION['usuario_nombre'] = $row['correo'];
+            $_SESSION['usuario_apellido'] = $row['contrasena'];
+            header("location:../index.html");
+            exit();
+          }
         } else {
-            $error_message = "Error en la consulta: " . $conexion->error;
+          ?>
+          <div class="error-message">
+            <p>Correo o contraseña incorrectos.</p>
+            <a href="inici.html">Volver a intentar</a>
+          </div>
+          <?php
         }
+      } else {
+        ?><div class="error-message">
+            <p>Error al conectar con la base de datos.</p>
+            <a href="inici.html">Volver a intentar</a>
+          </div>
+          <?php
+      }
     }
-}
+  }
+  
+  
+  
+
 ?>
 
         
