ce92f3eeacbb6526dad14e1b4f3f0bea258f872a
Destrowashere <felixsantiagosantafe2017@gmail.com>
2023-12-04 11:04:22 -0500

    aaaaa

--- Php_Base/crud/eliminar.php
+++ Php_Base/crud/eliminar.php
@@ -3,10 +3,12 @@ include_once('conexcrud.php');
 
 $id = $_GET['Id'];
 
-$sql_delete_claves = "DELETE FROM claves WHERE id_Cliente = '$id'";
-$query_delete_claves = mysqli_query($conex, $sql_delete_claves);
 
-if ($query_delete_claves) {
+$sql_delete_roles = "DELETE FROM roles WHERE id_Cliente = '$id'";
+$query_delete_roles = mysqli_query($conex, $sql_delete_roles);
+
+if ($query_delete_roles) {
+   
     $sql_delete_clientes = "DELETE FROM clientes WHERE id_Cliente = '$id'";
     $query_delete_clientes = mysqli_query($conex, $sql_delete_clientes);
 
