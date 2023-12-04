6ba744a6a9be98fe8994e9e10e196e6ada826767
ManuelFBejaranoAA <manubejaardi17@gmail.com>
2023-12-04 12:59:39 -0500

    logout 1

--- Mi cuenta/update_data.php
+++ Mi cuenta/update_data.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
-include('db.php');
+include('../conexion.php');
 
-$username = $_POST['username'];
-$email = $_POST['email'];
+$username = $_POST['Nombre'];
+$email = $_POST['Correo'];
 
-$sql = "UPDATE users SET username='$username', email='$email' WHERE id='".$_SESSION['id']."'";
+$sql = "UPDATE Cliente SET username='$Nombre', email='$Correo' WHERE id='".$_SESSION['id']."'";
 
 if (mysqli_query($conn, $sql)) {
-    $_SESSION['username'] = $username;
-    $_SESSION['email'] = $email;
+    $_SESSION['Nombre'] = $username;
+    $_SESSION['Correo'] = $email;
     header('Location: config.php');
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
