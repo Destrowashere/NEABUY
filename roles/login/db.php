2696f5910bd67550a43b8d163dd6401134917cc4
ManuelFBejaranoAA <manubejaardi17@gmail.com>
2024-06-03 14:41:24 -0500

    Roles parte 1

---
+++ roles/login/db.php
@@ -0,0 +1,5 @@
+<?php
+
+$conexion=mysqli_connect("localhost","root","12345678","login");
+
+?>
---
+++ roles/login/validar.php
@@ -0,0 +1,30 @@
+<?php
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+session_start();
+$_SESSION['usuario']=$usuario;
+
+$conexion=mysqli_connect("localhost","root","","nearbuybonitoo");
+
+$consulta="SELECT*FROM claves where Correo='$usuario' and contrasena='$contraseña'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_fetch_array($resultado);
+
+if($filas['id_cargo']==1){ //administrador
+    header("location:admin.php");
+
+}else
+if($filas['id_cargo']==2){ //cliente
+header("location:cliente.php");
+}
+else{
+    ?>
+    <?php
+    include("index.html");
+    ?>
+    <h1 class="bad">ERROR EN LA AUTENTIFICACION</h1>
+    <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
