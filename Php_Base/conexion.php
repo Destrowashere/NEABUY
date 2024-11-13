44036771181f69bdcbf23ce91d5e6c4fa6a2d8f4
Destrowashere <137834739+Destrowashere@users.noreply.github.com>
2024-11-12 21:33:30 -0500

    XD

--- Php_Base/conexion.php
+++ Php_Base/conexion.php
@@ -1,4 +1,4 @@
 <?php
 
-$conex = mysqli_connect("sql305.infinityfree.com", "if0_36548430", "VDYd0Ykr6H4i", "if0_36548430_nearbuybonitoo"); 
+$conex = mysqli_connect("localhost", "root", "", "nearbuybonitoo"); 
 ?>
--- Php_Base/crud/crud.php
+++ Php_Base/crud/crud.php
@@ -15,7 +15,8 @@
     </div>
 
     <div class="container  bg-light p-3 border border-dark rounded">
-        <h1 class="text-center">lista usuarios</h1>
+    <button onclick="generarPDF()">Descargar PDF</button><br>
+    <div id="chat"><h1 class="text-center">lista usuarios</h1> 
         <table class="table">
   <thead class= "table-dark">
 
@@ -44,7 +45,7 @@ while ($fila = mysqli_fetch_array($query)) {
         <td><?php echo $fila['Telefono']; ?></td>
         <td><?php echo $fila['Direccion']; ?></td>
         <td><?php echo $fila['Cedula']; ?></td>
-          <th scope="row">
+          <th scope="row"> </div>
             <a href="editar.php?Id=<?php
 echo $fila['id_Cliente']
 ?>" class="btn btn-warning" >Editar datos</a>
@@ -64,5 +65,35 @@ echo $fila['id_Cliente']
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
+<script>function generarPDF() {
+  // Selecciona el contenedor del chat
+  const chatElement = document.getElementById("chat");
+
+  // Configura html2pdf para capturar el contenedor con todos sus estilos visibles
+  html2pdf()
+    .from(chatElement)
+    .set({
+      margin: 1,
+      filename: 'chat.pdf',
+      html2canvas: { 
+        scale: 2,             // Mejora la calidad de la captura
+        useCORS: true,        // Permite capturar recursos externos
+        allowTaint: true,     // Permite cargar imágenes externas
+        windowWidth: chatElement.scrollWidth, // Ancho del área a capturar
+        windowHeight: chatElement.scrollHeight // Alto del área a capturar
+      },
+      jsPDF: { 
+        unit: 'in', 
+        format: 'a4', 
+        orientation: 'portrait' 
+      }
+    })
+    .save()
+    .catch((error) => {
+      console.error("Error al generar el PDF:", error);
+    });
+}
+</script>
   </body>
 </html>
